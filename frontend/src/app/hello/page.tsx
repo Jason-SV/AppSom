bdda3f00df2dbe5a456f5f1106cf0bee3c5deeac
unknown <jasonchawsuan@gmail.com>
2024-06-07 15:07:18 +0700

    init

---
+++ frontend/src/app/hello/page.tsx
@@ -0,0 +1,11 @@
+export default function HelloPage() {
+    return (
+        <main>
+            <div>
+                <h1>
+                    Hello Welcome
+                </h1>
+            </div>
+        </main>
+    )
+}
